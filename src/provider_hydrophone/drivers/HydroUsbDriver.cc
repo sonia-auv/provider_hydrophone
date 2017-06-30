23cbc61218093eaafcb3fd9f912676d810750131
Marc-Antoine Couture <coumarc9@outlook.com>
2017-06-30 12:05:03 -0400

    Adding HydroUsbDriver class

---
+++ src/provider_hydrophone/drivers/HydroUsbDriver.cc
@@ -0,0 +1,19 @@
+//
+// Created by coumarc9 on 6/30/17.
+//
+
+#include "HydroUsbDriver.h"
+
+namespace provider_hydrophone
+{
+
+    HydroUsbDriver::HydroUsbDriver() {
+
+    }
+
+    HydroUsbDriver::~HydroUsbDriver() {
+
+    }
+
+
+}
---
+++ src/provider_hydrophone/drivers/HydroUsbDriver.h
@@ -0,0 +1,19 @@
+//
+// Created by coumarc9 on 6/30/17.
+//
+
+#ifndef PROVIDER_HYDROPHONE_HYDROUSBDRIVER_H
+#define PROVIDER_HYDROPHONE_HYDROUSBDRIVER_H
+
+namespace provider_hydrophone
+{
+    class HydroUsbDriver {
+
+    public:
+
+        HydroUsbDriver();
+        ~HydroUsbDriver();
+
+    };
+}
+#endif //PROVIDER_HYDROPHONE_HYDROUSBDRIVER_H
