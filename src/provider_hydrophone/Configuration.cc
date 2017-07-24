1fcb2f779a1d0bb659b8806e27af63343e954adb
Marc-Antoine Couture <coumarc9@outlook.com>
2017-07-24 12:03:05 -0700

    Adding configuration class

---
+++ src/provider_hydrophone/Configuration.cc
@@ -0,0 +1,17 @@
+//
+// Created by coumarc9 on 7/24/17.
+//
+
+#include "Configuration.h"
+
+namespace provider_hydrophone
+{
+
+    Configuration::Configuration() {
+
+    }
+
+    Configuration::~Configuration() {
+
+    }
+}
---
+++ src/provider_hydrophone/Configuration.h
@@ -0,0 +1,27 @@
+//
+// Created by coumarc9 on 7/24/17.
+//
+
+#ifndef PROVIDER_HYDROPHONE_CONFIGURATION_H
+#define PROVIDER_HYDROPHONE_CONFIGURATION_H
+
+namespace provider_hydrophone
+{
+    class Configuration {
+
+    public:
+
+        Configuration();
+        ~Configuration();
+
+    private:
+
+
+
+    };
+}
+
+
+
+
+#endif //PROVIDER_HYDROPHONE_CONFIGURATION_H
