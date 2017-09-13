e65f600a86cf2b65ec645c7b003643937846a55a
olavoie <olavoie9507@gmail.com>
2017-09-13 14:46:21 -0400

    sim hydro

---
+++ scripts/sim_hydro.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+import random
+
+from provider_hydrophone.msg import PingMsg
+
+
+def sim_power():
+    rospy.init_node('sim_power', anonymous=True)
+    pub = rospy.Publisher('/provider_hydrophone/ping_msg', PingMsg, queue_size=10)
+    rate = rospy.Rate(1)  # 10hz
+    msg = PingMsg()
+    while not rospy.is_shutdown():
+
+        msg.amplitude = random.uniform(0, 100)
+        msg.frequency = random.uniform(20, 40)
+        msg.heading = random.uniform(0, 360)
+        msg.elevation = random.uniform(0, 90)
+        msg.noise = random.uniform(30, 40)
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        sim_power()
+    except rospy.ROSInterruptException:
+        pass
